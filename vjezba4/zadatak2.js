2be7c182abf41db7789d3ed8c84304444b9ef762
Ajna Mujkić <amujkic1@etf.unsa.ba>
2023-12-03 15:09:00 +0100

    Create zadatak2.js

---
+++ vjezba4/zadatak2.js
@@ -0,0 +1,7 @@
+function promijeniPozadinuTeksta(){
+    // Selektujemo paragraf po ID-ju
+    var paragrafElement = document.getElementById("paragraf");
+
+    // Postavljamo boju pozadine teksta na crvenu
+    paragrafElement.style.backgroundColor = "red";
+}
